04428b37f23cb01a4e5ba3d0bf5f4ac2e4490d77
Abiola Olaniran <abiola@gamsole.com>
2024-01-03 09:49:54 +0100

    init hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+  return (
+    <div className="">
+        <video poster="stellar.png" muted className="absolute top-0 right-0 left-0 bottom-0 h-screen w-screen object-cover" autoPlay={true} playsInline loop controls={false} src="https://player.vimeo.com/progressive_redirect/playback/826898904/rendition/1080p/file.mp4?loc=external&signature=7defb7b5e1b1b2029a27bf2ed10b8d69e26e90b3bf2d151b5ad3f9f9ef209246"></video>
+        <ul className="fixed mx-2 text-white space-y-3 text-[.8rem] bg-transparent left-5 w-[250px] bottom-7">
+          <li className="list-disc">GROWING LIFE ANYWHERE</li>
+          <p className="list-disc">WE REVOLUTIONIZE PRODUCTION OF PLANT-BASED INGREDIENTS ON EARTH AND IN SPACE.</p>
+        </ul>
+    </div>
+  )
+}
+
+export default Hero
