3babc78b5da0a495a6f9a613e247fdf58dcf923c
NiranMike <olaniranmicheal2020@gmail.com>
2024-01-23 12:33:30 +0100

    change

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -4,10 +4,10 @@ const SecondSection = () => {
   
   
   return (
-    <div className=" z-30 bg-[#F3F1F1] ">
-      <div className="grid lg:grid-cols-2 pb-[11rem] overflow-hidden">
+    <div className=" z-30 2xl:h-screen bg-[#F3F1F1] ">
+      <div className="grid lg:grid-cols-2 2xl:h-screen pb-[11rem] overflow-hidden">
 
-          <p className="text-[#9E9E9E] pt-20 pl-5 lg:leading-[3.7rem] text-[2rem] font-normal lg:text-[4rem]"><span className="text-black">Interstellar Lab.</span>
+          <p className="text-[#9E9E9E] 2xl:pt-[7rem] pt-20 pl-5  2xl:leading-[6.3rem] lg:leading-[3.7rem] text-[2rem] 2xl:text-[5rem] font-normal lg:text-[3rem]"><span className="text-black">Interstellar Lab.</span>
             We develop and commercialize biofarming platforms combining 
             AI, advanced hardware and bioscience to supply plant based ingredients at scale
             on Earth and in Space.
